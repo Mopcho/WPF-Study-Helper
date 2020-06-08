506c84d084ba3b2339df9ad5feba66ed9f4c0688
Mopcho <valerkatapoower@abv.bg>
2020-06-08 22:13:18 +0300

    Adding Funcionality

--- StudyHelper/MainWindow.xaml.cs
+++ StudyHelper/MainWindow.xaml.cs
@@ -12,18 +12,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace StudyHelper
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public bool isPause = false;
+
+        public bool isStarted = false;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            //  DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            //   {
+            //       this.Time.Text = DateTime.Now.Hour.ToString() + " : " + DateTime.Now.Minute.ToString();
+            //   }, this.Dispatcher);
+            
+        }
+        //bugged
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isPause)
+            {
+                this.isPause = false;
+
+                DateTime date = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, DateTime.Now.Hour, 0, 0);
+
+                DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+                {
+
+                    date = date.AddSeconds(1);
+                    this.Timer.Text = date.Minute + " : " + date.Second.ToString();
+
+
+                }, this.Dispatcher);
+                
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.isPause = true;
         }
     }
 }
